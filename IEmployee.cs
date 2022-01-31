795dca6ad8a6c0f1f5e9f22d1d7571446576d1d5
James Conlin <jacon.dev@outlook.com>
2022-01-31 16:14:06 +0000

    initial commit

---
+++ IEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceSegregation
+{
+    public interface IEmployee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public decimal AnnualSalary { get; set; }
+        public List<string> Benefits { get; set; }
+        public decimal CalculateSalary();
+
+    }
+
+    public class FullTimeEmployee : IEmployee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public decimal AnnualSalary { get; set; }
+        public List<string> Benefits { get; set; }
+        public decimal CalculateSalary()
+        {
+            return AnnualSalary / 12;
+        }
+    }
+}
